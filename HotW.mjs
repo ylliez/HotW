109f3fb508ef3cf18b367d17e5b4f600739e7b15
ylliez <illiez@hotmail.com>
2023-04-17 17:24:41 -0400

    trying with S2...

--- HotW.mjs
+++ HotW.mjs
@@ -9,8 +9,6 @@ const __dirname = dirname(__filename);
 
 const app = express();
 const port = process.env.PORT;
-// if (port == null || port == "") { port = 4200; }
-// const port = 4200;
 const server = http.createServer(app);
 const io = new SocketIO(server);
 server.listen(port, () => { console.log('server listening on port ' + port); })
@@ -23,7 +21,7 @@ io.on("connection", (socket) => {
     socket.on("buttonPress", () => {
         console.log("PRESSED!");
         launchProcess();
-        io.emit("S2C", "PRESSED!");
+        // io.emit("S2C", "RECEIVED!");
     });
     socket.on("disconnect", () => { console.log("DISCONNECTED..."); });
 });
@@ -41,20 +39,18 @@ import five from 'johnny-five';
 import NodeWebcam from "node-webcam";
 import recognize from 'tesseractocr';
 import mic from 'mic';
-import usb from 'usb';
 
 const modelT2I = await replicate.model("stability-ai/stable-diffusion:f178fa7a1ae43a9a9af01b833b9d2ecf97b1bcb0acfd2dc5dd04895e042863f1")
 const modelI2T = await replicate.model("methexis-inc/img2prompt:50adaf2d3ad20a6f911a8a9e3ccf777b263b8596fbd2c8fc26e8888f8a0edbb5");
 const modelT2S = await replicate.model("afiaka87/tortoise-tts:e9658de4b325863c4fcdc12d94bb7c9b54cbfe351b7ca1b36860008172b91c71")
 const modelS2T = await replicate.model("openai/whisper:e39e354773466b955265e969568deb7da217804d8e771ea8c9cd0cef6591f8bc")
-let predictionT2I, outputT2I, predictionI2T, predictionT2S, outputT2S, predictionS2T, outputS2T;
-let outputI2T = "an angry hamster eating brie in an impressionist style";
-// const device = new USB(10473, 649);
-// const thermalPrinter = new escpos.Printer(device);
+let predictionT2I, outputT2I, predictionI2T, outputI2T, predictionT2S, outputT2S, predictionS2T, outputS2T;
+const device = new USB(10473, 649);
+const thermalPrinter = new escpos.Printer(device);
 let board, button, led;
 let unimpressed = true, delighted = true;
 let iter = 1, step = 1;
-let filepath, fileiter, filestep;
+let filepath, fileiter;
 
 function launchProcess() {
     console.log(`LOADING...`)
@@ -62,7 +58,7 @@ function launchProcess() {
     fs.mkdir(`HotW_test/${timestamp}`, (err) => { if (err) { return console.error(err); } });
     filepath = `HotW_test/${timestamp}/`;
     fileiter = `${filepath}${iter}`;
-    filestep = `${fileiter}_${step}`;
+
     // let filepath = `app/${timestamp}_${i}`
     // console.log(filepath);
 
@@ -81,7 +77,7 @@ function launchProcess() {
                     delighted = false;
                     doTheThing();
                 } else {
-                    sound.play("buzzer.mp3");
+                    sound.play("buzzer.mp3", 0.1);
                     console.log("<buzzer.mp3>")
                 }
             }
@@ -91,7 +87,6 @@ function launchProcess() {
 
 function incrementStepAndTurnLedOn() {
     step++;
-    filestep = `${fileiter}_${step}`
     led.on();
     delighted = true;
 }
@@ -106,216 +101,219 @@ function incrementIterAndResetStep() {
 async function doTheThing() {
     switch (step) {
         case 1:
-            runT2I();
+            S2T2I();
             break;
         case 2:
-            runI2T();
+            I2T();
             break;
-        // case 3:
-        //     runT2S();
-        //     break;
-        // case 4:
-        //     stepS2T();
-        //     break;
         case 3:
-            playAudio();
-            break;
-        case 4:
-            stepS2T();
+            T2S();
             break;
     }
 }
 
-async function runT2I() {
-    console.log("PREDICTING T2I...");
-    predictionT2I = await modelT2I.predict({
-        prompt: outputI2T,
-        width: 768,
-        height: 512
-    });
-    outputT2I = predictionT2I.output[0];
-    console.log(outputT2I)
-    await getImage(outputT2I)
+function S2T2I() {
+    S2TrecordSpeech();
 }
 
-async function getImage(url) {
-    console.log(`FETCHING IMAGE...`)
-    const response = await fetch(url);
-    const blob = await response.blob();
-    const arrayBuffer = await blob.arrayBuffer();
-    const buffer = Buffer.from(arrayBuffer);
-    const write = await writeImage(buffer);
-}
-
-function writeImage(buffer) {
-    return new Promise((resolve, reject) => {
-        console.log(`WRITING IMAGE...`)
-        fs.writeFile(filestep + `.png`, buffer, function (err, res) {
-            resolve(convertImage());
-            if (err) reject(err);
-        });
-    })
-}
-
-function convertImage() {
-    return new Promise((resolve, reject) => {
-        console.log(`CONVERTING IMAGE...`)
-        let convertBuffer = fs.readFileSync(filestep + `.png`)
-        pngToJpeg({ quality: 90 })(convertBuffer)
-            .then(output => fs.writeFile(filestep + `.jpg`, output, function (err, res) {
-                resolve(printImageUSB());
-                if (err) reject(err);
-            }));
-    })
-}
-
-function printImageUSB() {
-    console.log(`PRINTING IMAGE...`)
-    var printer = new Printer('HP_Photosmart_5510_series');
-    var fileBuffer = fs.readFileSync(filestep + `.jpg`);
-    var options = { o: ' media=4x6.Photo.FullBleed' };
-    var jobFromBuffer = printer.printBuffer(fileBuffer, options);
-    printer.destroy()
-    setTimeout(function () { incrementStepAndTurnLedOn(); }, 15000);
-}
-
-async function runI2T() {
-    console.log("CAPTURING...");
-    NodeWebcam.capture(filestep + `.jpg`, { device: 'HD USB Camera', callbackReturn: "base64" }, async function (err, data) {
-        console.log("PREDICTING I2T...");
-        predictionI2T = await modelI2T.predict({ image: data });
-        outputI2T = predictionI2T.output.trim();
-        console.log(outputI2T)
-        await getPrompt(outputI2T)
-    });
-}
-
-async function getPrompt(text) {
-    console.log(`SAVING...`)
-    console.log(`PRINTING...`) // fix this
-    fs.writeFile(filestep + `.txt`, text, function (err, res) {
-        // printPrompt(text);
-        predictT2S(text);
-    });
-}
-
-// function printPrompt(text) {
-//     // console.log(`PRINTING...`)
-//     device.open(function (error) {
-//         thermalPrinter
-//             .size(1, 1)
-//             // .size(0.5, 0.5)
-//             .text(text)
-//             .feed()
-//             .feed()
-//             // .cut()
-//             .close();
-//     });
-//     setTimeout(function () { incrementStepAndTurnLedOn(); }, 2000);
-// }
-
-// async function runT2S() {
-//     console.log("CAPTURING...");
-//     NodeWebcam.capture("webcam_testOCR", { device: 'HD USB Camera', callbackReturn: "buffer" }, function (err, data) {
-//         recognizeOCR(data)
-//     })
-// }
-
-// async function recognizeOCR(data) {
-//     console.log("RECOGNIZING...");
-//     const resultOCR = await recognize(data)
-//     let textOCR = resultOCR.replace(/\n/g, "");
-//     console.log(textOCR)
-//     saveOCR(textOCR)
-// }
-
-// function saveOCR(text) {
-//     console.log(`SAVING...`)
-//     fs.writeFile(filestep + `.txt`, text, function (err, res) {
-//         predictT2S(text);
-//     });
-// }
-
-async function predictT2S(text) {
-    console.log("PREDICTING T2S...");
-    predictionT2S = await modelT2S.predict({
-        'text': text,
-        'voice_a': "random",
-    })
-    outputT2S = predictionT2S.output;
-    console.log(outputT2S)
-    await getAudio(outputT2S)
-}
-
-async function getAudio(url) {
-    const response = await fetch(url);
-    const blob = await response.blob();
-    const arrayBuffer = await blob.arrayBuffer();
-    const buffer = Buffer.from(arrayBuffer);
-    const write = await writeAudio(buffer);
-}
-
-function writeAudio(buffer) {
-    return new Promise((resolve, reject) => {
-        console.log(`SAVING...`)
-        fs.writeFile(filestep + `.mp3`, buffer, function (err, res) {
-            // resolve(playAudio());
-            resolve(incrementStepAndTurnLedOn());
-            if (err) reject(err);
-        });
-    })
-}
-
-function playAudio() {
-    console.log(`PLAYING...`)
-    sound.play(filestep + ".mp3");
-    setTimeout(function () { incrementStepAndTurnLedOn(); }, 10000);
-}
-
-function stepS2T() {
-    console.log("RECORDING...");
+function S2TrecordSpeech() {
     var micInstance = mic({
         debug: false,
-        fileType: 'wav'
+        fileType: 'mp3'
     });
     var micInputStream = micInstance.getAudioStream();
-    var outputFileStream = fs.WriteStream(filestep + '.wav');
+    var outputFileStream = fs.WriteStream(`${fileiter}-1.mp3`);
     micInputStream.pipe(outputFileStream);
     micInputStream.on('startComplete', function () {
-        console.log("Started recording");
-        setTimeout(function () {
-            micInstance.stop();
-        }, 15000);
+        setTimeout(function () { micInstance.stop(); }, 12000);
     });
     micInputStream.on('stopComplete', function () {
-        console.log("Stopped recording");
-        getRecording();
+        sound.play("assets/iphone_record_stop.mp3", 1.0)
+        S2TconvertSpeech();
     });
+    sound.play("assets/iphone_record_start.mp3", 1.0)
+    console.log("S2T | RECORDING SPEECH...");
     micInstance.start();
 }
 
-function getRecording() {
-    console.log("CONVERTING");
-    var audioWAV = filestep + '.wav';
-    let audioBase64 = fs.readFileSync(audioWAV, { encoding: 'base64' });
-    // console.log(audioBase64)
-    runS2T(audioBase64)
+function S2TconvertSpeech() {
+    console.log("S2T | CONVERTING SPEECH...");
+    let audiofile = `${fileiter}-1.mp3`;
+    let audioBase64 = fs.readFileSync(audiofile, { encoding: 'base64' });
+    S2TpredictText(audioBase64)
 }
 
-
-async function runS2T(speech) {
-    console.log("PREDICTING S2T...");
+async function S2TpredictText(speech) {
+    console.log("S2T | PREDICTING TEXT...");
     predictionS2T = await modelS2T.predict({
         'audio': `data:audio/mp3;base64,${speech}`,
         'temperature': 0,
     })
     outputS2T = predictionS2T.output.transcription
     console.log(outputS2T)
-    await getTranscription(outputS2T);
+    await S2TfetchText(outputS2T);
+    await T2IpredictImage(outputS2T);
 }
 
-async function getTranscription(text) {
-    console.log(`WRITING...`)
-    fs.writeFile(filestep + `.txt`, text, function (err, res) { });
-    incrementIterAndResetStep();
+async function S2TfetchText(text) {
+    console.log(`S2T | FETCHING TEXT...`)
+    fs.writeFile(`${fileiter}-2.txt`, text, function (err, res) { });
+}
+
+async function T2IpredictImage(text) {
+    console.log("T2I | PREDICTING IMAGE...");
+    predictionT2I = await modelT2I.predict({
+        prompt: text,
+        width: 768,
+        height: 512
+    });
+    outputT2I = predictionT2I.output[0];
+    console.log(outputT2I)
+    await T2IfetchImage(outputT2I)
+}
+
+async function T2IfetchImage(url) {
+    console.log(`T2I | FETCHING IMAGE...`)
+    const response = await fetch(url);
+    const blob = await response.blob();
+    const arrayBuffer = await blob.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+    const write = await T2IwriteImage(buffer);
+}
+
+function T2IwriteImage(buffer) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(`${fileiter}-3.png`, buffer, function (err, res) {
+            resolve(T2IconvertImage());
+            if (err) reject(err);
+        });
+    })
+}
+
+function T2IconvertImage() {
+    return new Promise((resolve, reject) => {
+        console.log(`T2I | CONVERTING IMAGE...`)
+        let convertBuffer = fs.readFileSync(`${fileiter}-3.png`)
+        pngToJpeg({ quality: 90 })(convertBuffer)
+            .then(output => fs.writeFile(`${fileiter}-4.jpg`, output, function (err, res) {
+                resolve(T2IprintImage());
+                if (err) reject(err);
+            }));
+    })
+}
+
+function T2IprintImage() {
+    console.log(`T2I | PRINTING IMAGE...`)
+    var printer = new Printer('HP_Photosmart_5510_series');
+    var fileBuffer = fs.readFileSync(`${fileiter}-4.jpg`);
+    var options = { o: ' media=4x6.Photo.FullBleed' };
+    var jobFromBuffer = printer.printBuffer(fileBuffer, options);
+    printer.destroy()
+    setTimeout(function () { incrementStepAndTurnLedOn(); }, 15000);
+}
+
+function I2T() {
+    I2TcaptureImage();
+}
+
+async function I2TcaptureImage() {
+    console.log("I2T | CAPTURING IMAGE...");
+    NodeWebcam.capture(`${fileiter}-5.jpg`, { device: 'HD USB Camera', callbackReturn: "base64" }, async function (err, data) {
+        // NodeWebcam.capture(`${fileiter}-5.jpg`, { callbackReturn: "base64" }, async function (err, data) {
+        I2TpredictText(data)
+    });
+}
+
+async function I2TpredictText(imageData) {
+    console.log("I2T | PREDICTING TEXT...");
+    predictionI2T = await modelI2T.predict({ image: imageData });
+    outputI2T = predictionI2T.output.trim();
+    console.log(outputI2T)
+    await I2TfetchText(outputI2T)
+}
+
+async function I2TfetchText(text) {
+    console.log(`I2T | FETCHING TEXT...`)
+    // console.log(`PRINTING...`) // fix this
+    fs.writeFile(`${fileiter}-6.txt`, text, function (err, res) {
+        I2TprintText(text);
+    });
+}
+
+function I2TprintText(text) {
+    console.log(`I2T | PRINTING TEXT...`)
+    device.open(function (error) {
+        thermalPrinter
+            .size(1, 1)
+            // .size(0.5, 0.5)
+            .text(text)
+            .feed()
+            .feed()
+            // .cut()
+            .close();
+    });
+    setTimeout(function () { incrementStepAndTurnLedOn(); }, 2000);
+}
+
+function T2S() {
+    T2ScaptureText();
+}
+
+async function T2ScaptureText() {
+    console.log("T2S | CAPTURING TEXT...");
+    // NodeWebcam.capture(`${fileiter}-7.jpg`, { device: 'HD USB Camera', callbackReturn: "buffer" }, function (err, data) {
+    // NodeWebcam.capture(`${fileiter}-7.jpg`, { callbackReturn: "buffer" }, function (err, data) {
+    NodeWebcam.capture(`${fileiter}-7.jpg`, { device: 'HD USB Camera', callbackReturn: "buffer" }, function (err, data) {
+        T2SrecognizeText(data)
+    });
+}
+
+async function T2SrecognizeText(data) {
+    console.log("T2S | RECOGNIZING TEXT...");
+    const resultOCR = await recognize(data)
+    let textOCR = resultOCR.replace(/\n/g, "");
+    console.log(textOCR)
+    T2SsaveText(textOCR)
+}
+
+function T2SsaveText(text) {
+    console.log(`T2S | FETCHING TEXT...`)
+    fs.writeFile(`${fileiter}-8.txt`, text, function (err, res) {
+        T2SpredictSpeech(text);
+    });
+}
+
+async function T2SpredictSpeech(text) {
+    console.log("T2S | PREDICTING SPEECH...");
+    predictionT2S = await modelT2S.predict({
+        'text': text,
+        'voice_a': "random",
+    })
+    outputT2S = predictionT2S.output;
+    console.log(outputT2S)
+    await T2SfetchSpeech(outputT2S)
+}
+
+async function T2SfetchSpeech(url) {
+    console.log(`T2S | FETCHING SPEECH...`)
+    const response = await fetch(url);
+    const blob = await response.blob();
+    const arrayBuffer = await blob.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+    const write = await T2SsaveSpeech(buffer);
+}
+
+function T2SsaveSpeech(buffer) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(`${fileiter}-9.mp3`, buffer, function (err, res) {
+            resolve(T2SplaySpeech());
+            if (err) reject(err);
+        });
+    })
+}
+
+function T2SplaySpeech() {
+    console.log(`T2S | PLAYING SPEECH...`)
+    sound.play(`${fileiter}-9.mp3`, 0.01);
+    setTimeout(function () { incrementIterAndResetStep(); }, 15000);
 }
